b74b91293e775f5865c87e0edb3801dfb9fb7926
vdtdg <valerian@dtdg.fr>
2025-03-27 22:37:32 +0100

    Script for release updated

--- make-zip.sh
+++ make-zip.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-zip_name="linkedin-feed-locker.zip"
+zip_name="linkedin-suggested-post-remover.zip"
 build_folder="target"
 
 mkdir -p $build_folder
 rm -f $build_folder/$zip_name
 
-zip -r $build_folder/$zip_name manifest.json content.js style.css icons/ LICENSE README.md
+zip -r $build_folder/$zip_name manifest.json content.js icons/ LICENSE README.md
 
 echo "Packaged extension as $build_folder/$zip_name"
