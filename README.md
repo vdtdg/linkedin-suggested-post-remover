31675dab8ea944491e66e4f7e86f37e49defc6e5
vdtdg <valerian@dtdg.fr>
2025-03-27 18:27:26 +0100

    Copy from linkedin feed locker

---
+++ README.md
@@ -0,0 +1,34 @@
+# LinkedIn Feed Locker Browser Extension
+A browser extension to lock the LinkedIn feed from the view, helping distracted person to remain **focused** with the option of still accessing the feed.
+
+Example of what it looks like with the extension:
+![The feed is locked and a button appears on top to unlock it](/docs/locked.png "The feed is locked and a button appears on top to unlock it")
+
+You can still press the button for 1.5 seconds to unlock the feed:
+![A progress bar appears on click](/docs/unlocking.png "A progress bar appears on click")
+
+People posting on LinkedIn are getting better and better every day at catching your attention (and mine!). 
+
+They post entrancing pictures and gripping hooks to get you to read their content. 
+
+And they are right to do so!
+
+But you also have to protect your attention.
+
+With this tool, your feed is locked by default, and you still have the opportunity to read it by pressing the unlock button.
+
+
+# Installation
+<a href="https://addons.mozilla.org/en-US/firefox/addon/linkedin-feed-locker/"><img src="https://user-images.githubusercontent.com/585534/107280546-7b9b2a00-6a26-11eb-8f9f-f95932f4bfec.png" alt="Get Linkedin Feed Locker for Firefox"></a>
+<a href="https://chromewebstore.google.com/detail/linkedin-feed-locker/ekbageagichmpplpgnbonflcdhpaoall"><img src="https://user-images.githubusercontent.com/585534/107280622-91a8ea80-6a26-11eb-8d07-77c548b28665.png" alt="Get Linkedin Feed Locker for Chromium"></a>
+
+
+# Features
+- Free, no ads.
+- The feed is not unlocked immediately, adding another resistance to distraction.
+- Uses LinkedIn styles for nice visual integration.
+- Works in Light and Dark modes.
+- Still works when navigating in LinkedIn back to the feed, not only on first launch.
+
+# Support
+If you encounter any problem, please raise an issue in this repository, I'll review it as soon as possible.
---
+++ content.js
@@ -0,0 +1,83 @@
+const feedSelector = 'div[data-finite-scroll-hotkey-context="FEED"]';
+const feedFollowSelector = ".feed-follows-module";
+const buttonId = "unlock-button";
+const buttonSelector = `#${buttonId}`;
+const lockTime = 2700; // in milliseconds
+
+function unlockFeed() {
+  const feed = document.querySelector(feedSelector);
+  const feedFollow = document.querySelector(feedFollowSelector);
+  const button = document.querySelector(buttonSelector);
+
+  feed.style.visibility = "visible";
+  feedFollow.style.visibility = "visible";
+  button.style.display = "none";
+}
+
+function setupUnlockButton() {
+  const feed = document.querySelector(feedSelector);
+
+  if (feed) {
+    const button = document.createElement("button");
+    button.setAttribute("id", buttonId);
+    button.textContent = "Feed locked. Press to unlock.";
+    button.classList.add("artdeco-card");
+    button.classList.add("t-black");
+    button.classList.add("t-16");
+    button.classList.add("t-bold");
+    feed.before(button);
+
+    const progress = document.createElement("progress");
+    progress.setAttribute("id", "progress-bar");
+    progress.setAttribute("max", "100");
+    progress.setAttribute("value", "0");
+
+    let pressTimer;
+    let interval;
+
+    function startProgress(event) {
+      if (event.button === 1 || event.button === 2) { // right or middle click
+        return;
+      }
+      button.textContent = "";
+      button.appendChild(progress);
+      progress.value = 0;
+      interval = setInterval(() => {
+        progress.value++;
+        if (progress.value >= 100) {
+          unlockFeed();
+          clearInterval(interval);
+        }
+      }, lockTime / 100);
+    }
+
+    function stopProgress() {
+      clearTimeout(pressTimer);
+      clearInterval(interval);
+      button.textContent = "Feed locked. Press to unlock.";
+      progress.value = 0;
+    }
+
+    // Mouse based device
+    button.addEventListener("mousedown", startProgress);
+    document.addEventListener("mouseup", stopProgress);
+
+    // Touch based device
+    button.addEventListener("touchstart", startProgress);
+    document.addEventListener("touchend", stopProgress);
+  }
+}
+
+function observePageChanges() {
+  setInterval(() => {
+    if (window.location.href.includes("/feed")) {
+      const button = document.querySelector(buttonSelector);
+      if (!button) {
+        setupUnlockButton();
+      }
+    }
+  }, 500);
+}
+
+document.addEventListener("DOMContentLoaded", setupUnlockButton);
+document.addEventListener("DOMContentLoaded", observePageChanges);
